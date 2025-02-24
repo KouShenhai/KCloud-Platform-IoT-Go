87297b0773bd829e440223f4bc7d37ffa1aab4ac
koushenhai <2413176044@qq.com>
2025-02-24 14:05:45 +0800

    feat: go对接excel

--- go.mod
+++ go.mod
@@ -4,12 +4,20 @@ go 1.22
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.5.0
+	github.com/xuri/excelize/v2 v2.9.0
 	go.uber.org/zap v1.27.0
 )
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.4 // indirect
+	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
+	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/net v0.27.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"KCloud-Platform-Go/core"
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
-
 type Post struct {
 	UserId int    `json:"userId"`
 	Id     int    `json:"id"`
@@ -15,17 +8,18 @@ type Post struct {
 }
 
 func main() {
-	var header = map[string]string{
-		"Content-Type": "application/json",
-	}
-	var param = map[string]string{
-		"id": "1",
-	}
-	body, err := core.SendRequest(http.MethodGet, "http://jsonplaceholder.typicode.com/posts", param, header)
-	if err != nil {
-		panic(err)
-	}
-	var b []Post
-	_ = json.Unmarshal(body, &b)
-	fmt.Println(b[0].Id)
+	//var header = map[string]string{
+	//	"Content-Type": "application/json",
+	//}
+	//var param = map[string]string{
+	//	"id": "1",
+	//}
+	//body, err := core.SendRequest(http.MethodGet, "http://jsonplaceholder.typicode.com/posts", param, header)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//var b []Post
+	//_ = json.Unmarshal(body, &b)
+	//fmt.Println(b[0].Id)
+
 }
