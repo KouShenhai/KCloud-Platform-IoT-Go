4ea1f7c66fedd8d5ee1144923b4c3189503ec44c
koushenhai <2413176044@qq.com>
2025-02-25 09:30:18 +0800

    feat: 初始化提交代码

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-type Post struct {
-	UserId int    `json:"userId"`
-	Id     int    `json:"id"`
-	Title  string `json:"title"`
-	Body   string `json:"body"`
-}
-
 func main() {
-	//var header = map[string]string{
-	//	"Content-Type": "application/json",
-	//}
-	//var param = map[string]string{
-	//	"id": "1",
-	//}
-	//body, err := core.SendRequest(http.MethodGet, "http://jsonplaceholder.typicode.com/posts", param, header)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//var b []Post
-	//_ = json.Unmarshal(body, &b)
-	//fmt.Println(b[0].Id)
 
 }
